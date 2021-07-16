03727f59e8718527883478748e217d4bf704b520
Marvin Bernd <hello@marvinbernd.de>
2021-07-16 15:03:14 +0200

    refactoring

---
+++ src/runner.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const fetchRegisterAPI = require("../utils/fetchRegisterAPI");
+
+class Runner {
+  constructor(tdgdir) {
+    this.tdgdir = tdgdir;
+    this.testcases = fs.readdirSync(tdgdir);
+  }
+
+  run() {
+    this.testcases.forEach(async (testcase) => {
+      const personPath = path.join(this.tdgdir, testcase, "runnerData.json");
+
+      if (fs.existsSync(personPath)) {
+        const PersonData = fs.readFileSync(personPath);
+        const person = JSON.parse(PersonData);
+
+        await fetchRegisterAPI(person)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(testcase, data.message);
+          });
+      }
+    });
+  }
+}
+
+module.exports.Runner = Runner;
---
+++ utils/fetchRegisterAPI.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+const fetchRegisterAPI = (data) => {
+  const response = fetch("http://localhost:3000/api/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response;
+};
+
+module.exports.fetchRegisterAPI = fetchRegisterAPI;
